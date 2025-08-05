0e10fef68207f615253652b381af125084bd9a78
bitsofberra <metesumeyyeberra@gmail.com>
2025-08-05 11:10:46 +0300

    Pulled yilmaz_subsriber.cpp from yilamz branch

---
+++ src/cpp_pubsub/src/yilmaz_subscriber.cpp
@@ -0,0 +1,44 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class yilmaz : public rclcpp::Node
+{
+
+
+public:
+
+    yilmaz()
+    :Node("andromeda")
+
+    {
+        auto topic_callback =
+
+        [this](std_msgs::msg::String::UniquePtr msg) -> void {
+
+
+            RCLCPP_INFO(this->get_logger(), "duydum ki sefere çıkmayı kuruyormuşsun : '%s'", msg->data.c_str());
+
+
+        };
+
+        subscription_ =
+            this->create_subscription<std_msgs::msg::String>("topic", 40, topic_callback);
+
+    };
+
+    
+private:
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
+};
+
+int main(int argc, char * argv[])
+{
+
+rclcpp::init(argc, argv);
+rclcpp::spin(std::make_shared<yilmaz>());
+rclcpp::shutdown();
+return 0;
+}
