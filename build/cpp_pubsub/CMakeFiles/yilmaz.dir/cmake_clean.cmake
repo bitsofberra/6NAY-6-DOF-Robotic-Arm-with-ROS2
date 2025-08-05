c81a59ade0fe4d04d88dbe25d10d8b9998f5022b
theonlyyilmaz <gulmezylmazz@gmail.com>
2025-08-05 11:01:51 +0300

    yilmaz_subscriber node

---
+++ build/cpp_pubsub/CMakeFiles/yilmaz.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yilmaz.dir/src/yilmaz_subscriber.cpp.o"
+  "CMakeFiles/yilmaz.dir/src/yilmaz_subscriber.cpp.o.d"
+  "yilmaz"
+  "yilmaz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yilmaz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/cpp_pubsub/CMakeFiles/yilmaz.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for yilmaz.
+# This may be replaced when dependencies are built.
